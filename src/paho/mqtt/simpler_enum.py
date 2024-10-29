2580b941e6327c45e8cd15a1339224574065c5b8
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-10-29 12:52:09 +0000

    Create simpler_enum.py

---
+++ src/paho/mqtt/simpler_enum.py
@@ -0,0 +1,15 @@
+
+class IntEnum:
+    pass
+    
+class Enum:
+    pass
+
+class _Auto:
+    def __init__(self):
+        self._i = -1
+    def __call__(self):
+        self._i += 1
+        return self._i
+
+auto = _Auto()
