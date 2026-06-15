7407a1e8b3e5c4a5f61cc55a71102b15c41567b0
James Parrott <80779630+JamesParrott@users.noreply.github.com>
2026-06-15 10:05:54 +0100

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -2,6 +2,11 @@ import paho.mqtt.client as mqtt
 
 TOPIC = "test/topic"
 
+class Client(mqtt.Client):
+    def _do_on_disconnect(self, *args, **kwargs):
+        print(f" _do_on_disconnect called: {args}, {kwargs}")
+        super()._do_on_disconnect(*args, **kwargs)
+
 def on_connect(client, userdata, flags, reason_code, properties=None):
     client.subscribe(TOPIC)
 
@@ -18,7 +23,11 @@ client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_message = on_message
 client.on_disconnect = on_disconnect
+client.on_log = print
 
 # client.connect("localhost", 1883, keepalive=30)
-client.connect("192.168.137.1", 1883, keepalive=4)
+client.connect("192.168.137.1", 1883, keepalive=10)
+#while client.is_connected:
+#    client.loop_read()
+
 client.loop_forever()
